cd23b0cbf6ffa491bf19a6884c9703348b5ea8d7
Terry Hebert <trryhebert@gmail.com>
2014-02-02 14:16:36 -0400

    Added a '?' on the Bool of isActive and added a call to ErrorMsg when I hit an sign in error

--- shareurstorydb/AccountModels.cs
+++ shareurstorydb/AccountModels.cs
@@ -29,7 +29,7 @@ namespace shareurstorydb
         [Required]
         public string UserName { get; set; }
         [DefaultValue(true)]
-        public bool isActive { get; set; }
+        public bool? isActive { get; set; }
     }
 
     public class RegisterExternalLoginModel
