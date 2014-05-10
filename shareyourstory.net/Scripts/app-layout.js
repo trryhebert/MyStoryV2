f5ff8ad13ea1f6664d5b561edba2339d128f0a39
Isabelle <i_goguen@hotmail.com>
2014-05-10 14:25:15 -0300

    Recovered js files.

---
+++ shareyourstory.net/Scripts/app-layout.js
@@ -0,0 +1,21 @@
+﻿$(window).load(function () {
+    footerPosition();
+});
+
+$(window).resize(function () {
+    footerPosition();
+});
+
+function footerPosition() {
+    var hWindow = $(window).height(); // get the height of your content
+    var hContent = $(document).height();  // get the height of the visitor's browser window
+
+    if (hContent <= hWindow) {
+        $('.copyrights').css('position', 'fixed');
+        $('.copyrights').css('bottom', '0');
+    }
+    else {
+        $('.copyrights').css('position', 'relative');
+        $('.copyrights').css('bottom', 'auto');
+    }
+}
